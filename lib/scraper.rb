602332a52af6f563e3117f3927292f4ebc02704e
Matt Farmer <mrfarmer777@gmail.com>
2018-06-04 03:31:39 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,14 +4,25 @@ require 'nokogiri'
 
 class Scraper
 
-
+  #student: .student-card
+  #student name: .student-card .card-text-container .student-name
+  #student name: .student-card .card-text-container .student-location
+  #student profile url .student-card a (get attrinbute, href)
 
 
   def self.scrape_index_page(index_url)
+    scraped_stus = []
     html=open(index_url)
     doc=Nokogiri::HTML(html)
+    student_data=doc.css(".student-card")
     binding.pry
-
+    student_data.each do |student|
+      name = student.css('.card-text-container .student-name').text
+      location=student.css('.card-text-container .student-location').text
+      stu_hash={name: name, location:location}
+      scraped_stus<<stu_hash
+    end    
+    scraped_stus
   end
 
   def self.scrape_profile_page(profile_url)
