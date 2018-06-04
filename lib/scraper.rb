8f86ccbaa37637cd3277dc72abf8c7eeb7742b71
Matt Farmer <mrfarmer777@gmail.com>
2018-06-04 03:17:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html=open_uri(index_url)
+    html=open(index_url)
     doc=Nokogiri::HTML(html)
     binding.pry
   end
