070c91a2e706811753315e561b28975203be8092
Matt Farmer <mrfarmer777@gmail.com>
2018-06-04 03:41:59 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,6 +19,7 @@ class Scraper
     student_data.each do |student|
       name = student.css('.card-text-container .student-name').text
       location=student.css('.card-text-container .student-location').text
+      profile_url=student.css('a').attribute("href").text
       #binding.pry
       stu_hash={name: name, location:location}
       scraped_stus<<stu_hash
